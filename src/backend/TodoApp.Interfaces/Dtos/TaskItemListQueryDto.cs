0e00dec39afce91ad8442e3d121022f3b49913c2
Oksana Vakuliak <oksanabagrova19@gmail.com>
2026-06-06 10:56:04 +0300

    fix: harden task query validation

--- src/backend/TodoApp.Interfaces/Dtos/TaskItemListQueryDto.cs
+++ src/backend/TodoApp.Interfaces/Dtos/TaskItemListQueryDto.cs
@@ -4,12 +4,13 @@ namespace TodoApp.Interfaces.Dtos;
 
 public sealed class TaskItemListQueryDto
 {
-    [Range(1, int.MaxValue)]
+    [Range(1, 1_000_000)]
     public int Page { get; init; } = 1;
 
     [Range(1, 100)]
     public int Limit { get; init; } = 20;
 
+    [StringLength(200)]
     public string? Search { get; init; }
 
     public Guid? CategoryId { get; init; }
